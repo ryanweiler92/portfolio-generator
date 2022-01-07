c35619c1962666c945051a3c63ba05521e6b0664
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-07 15:55:34 -0500

    inquirer

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers=>console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Checkout index.htrml to see the output!')
-});
+//     console.log('Portfolio complete! Checkout index.htrml to see the output!')
+// });
 
