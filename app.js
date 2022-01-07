5b5cf232fbed67134f5a40795eb3142f9b47557a
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-07 14:19:04 -0500

    modularized

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem)=> console.log(profileItem))
-};
 
-printProfileData(profileDataArgs)
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Checkout index.htrml to see the output!')
+});
+
