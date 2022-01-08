e23c80916116845cf7117f52a6d6ee55491d7795
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-08 08:54:08 -0500

    added question validation

--- app.js
+++ app.js
@@ -7,12 +7,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true
+              } else {
+                console.log('Please enter your name!');
+                return false;
+              }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: nameInput => {
+              if (nameInput) {
+                return true 
+              } else {
+                console.log('Please enter your GitHub Username!')
+                return false;
+              }
+            } 
         },
         {
             type: 'input',
@@ -37,12 +53,28 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true
+          }else{
+            console.log("Please enter a project name!")
+            return false
+          }
+        } 
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+        if (nameInput) {
+          return true
+        } else {
+          console.log('Please enter a description!')
+          return false
+        }
+      }
       },
       {
         type: 'checkbox',
@@ -53,7 +85,15 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+          if (nameInput){
+            return true
+          } else {
+            console.log('Please enter a GitHub link!')
+            return false
+          }
+        }
       },
       {
         type: 'confirm',
